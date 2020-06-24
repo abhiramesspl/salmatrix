44bcae3a96206d0359fb909afec5f8f4ce776b35
Dibyanshu Banerjee <dibyanshu@esspl.com>
2020-06-24 15:53:27 +0530

    New Design Change

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,17 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+
+
+
+export interface UserData {
+  id: string;
+  name: string;
+  mgenerate: string;
+  cdate:string;
+  fdate:string;
+}
+/** Constants used to fill up our data base. */
+
+const NAMES: string[] = [
+  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
+
+const CDATES: string[] = [
+  '11-06-2019', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020',
+  '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020'
+];
+
+const FDATES: string[] = [
+  '11-06-2019', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020',
+  '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020', '12-06-2020'
+];
+
+const MGENERATED: string[] = [
+  'Yes', 'Yes', 'Yes',  'Yes','No', 'No', 'Yes'
+];
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
+
+
+
+
 export class DashboardComponent implements OnInit {
-  
   languages: any= ['EN', 'FR'];
   selected:any = 'EN';
-  constructor() { }
+  
+  displayedColumns: string[] = ['id', 'name',  'cdate', 'fdate', 'mgenerate'];
+  dataSource: MatTableDataSource<UserData>;
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor() {
+     // Create 100 users
+     const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+     // Assign the data to the data source for the table to render
+     this.dataSource = new MatTableDataSource(users);
+   }
+
+  ngOnInit(){
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }}
+
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+  const cdate = CDATES[Math.round(Math.random() * (CDATES.length - 1))];
+
+  const fdate = FDATES[Math.round(Math.random() * (FDATES.length - 1))];
+  const mgenerate = MGENERATED[Math.round(Math.random() * (MGENERATED.length - 1))];
+
+  return {
+    id: id.toString(),
+    name: name,
+    cdate: cdate,
+    mgenerate: mgenerate,
+    fdate:fdate
+  };
 }
+
+
