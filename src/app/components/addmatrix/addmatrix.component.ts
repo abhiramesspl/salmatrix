b502303810813fa2bbbeb3891522a11f66a8c531
Dibyanshu Banerjee <dibyanshu@esspl.com>
2020-06-26 18:38:23 +0530

    Matrix section

--- src/app/components/addmatrix/addmatrix.component.ts
+++ src/app/components/addmatrix/addmatrix.component.ts
@@ -1,16 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSelectModule} from '@angular/material/select';
+interface Food {
+  value: string;
+  viewValue: string;
+}
+interface Transportation {
+  value: string;
+  viewValue: string;
+}
+interface Housing {
+  value: string;
+  viewValue: string;
+}
+interface Healthcare {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-addmatrix',
   templateUrl: './addmatrix.component.html',
   styleUrls: ['./addmatrix.component.css'],
   
 })
+
+
+
 export class AddmatrixComponent implements OnInit {
-  languages: any= ['EN', 'FR'];
-  selected:any = 'EN';
+
+  foods: Food[] = [
+    {value: 'yes-0', viewValue: 'Yes'},
+    {value: 'No-1', viewValue: 'No'},
+  ];
+
+  transportation: Transportation[] = [
+    {value: 'yes-0', viewValue: 'Yes'},
+    {value: 'No-1', viewValue: 'No'},
+  ];
+
+  housing: Housing[] = [
+    {value: 'yes-0', viewValue: 'Yes'},
+    {value: 'No-1', viewValue: 'No'},
+  ];
+
+  healthcare: Healthcare[] = [
+    {value: 'yes-0', viewValue: 'Yes'},
+    {value: 'No-1', viewValue: 'No'},
+  ];
+  
+  panelOpenState = false;
   
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
